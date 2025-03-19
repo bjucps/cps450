086574b843569e517dea71a909dd44ef05569fbe
Sarah Gothard <sgothard@bju.edu>
2025-03-19 10:35:10 -0400

    write_int example

--- examples/codegen/2025/loopy.c
+++ examples/codegen/2025/loopy.c
@@ -1,15 +1,14 @@
-#include <unistd.h>
+void writeint(int num);
+
 int main(int argc, char **argv) {
     int x = 3;
     if (x > 0) {
         while (x > 0) {
-            char c = x + '0';
-            write(1, &c, 1);
+            writeint(x);
             --x;
         }
     } else {
-        char c = '9';
-        write(1, &c, 1);
+        writeint(999);
     }
     
 }
