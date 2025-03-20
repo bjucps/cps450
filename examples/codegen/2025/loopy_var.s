ac8a4079b7eacf9656c8f7e2677525559e46f225
Sarah Gothard <sgothard@bju.edu>
2025-03-20 08:39:16 -0400

    Correct use of Global Offset Table

--- examples/codegen/2025/loopy_var.s
+++ examples/codegen/2025/loopy_var.s
@@ -1,4 +1,4 @@
-# Compile: gcc -m32 -no-pie loopy_var.s stdlib.o
+# Compile: gcc -m32 loopy_var.s stdlib.o
 # dream source
 # class loopy is
 #   x: int  
@@ -26,23 +26,23 @@ main:						# standard function prologue:
 	pushl	%ebx			# push bx
 	pushl	%ecx			# push sp+4
 	subl	$32, %esp						# reserve space on the stack. Stack grows down.
-	call	__x86.get_pc_thunk.ax			# put ip in ax. See https://courses.cs.vt.edu/cs3214/spring2022/questions/pcmaterialization		
-	addl	$_GLOBAL_OFFSET_TABLE_, %eax	# set ax to _GLOBAL_OFFSET_TABLE_ + ip of <main+23>
-	movl	$3, _x			# _x := 3
+	call	__x86.get_pc_thunk.bx			# put ip in bx. See https://courses.cs.vt.edu/cs3214/spring2022/questions/pcmaterialization		
+	addl	$_GLOBAL_OFFSET_TABLE_, %ebx	# set bx to _GLOBAL_OFFSET_TABLE_ + ip of <main+23>
+	movl	$3, _x@GOTOFF(%ebx)	# _x := 3
 							# if (x > 0) then
-	cmpl	$0, _x			# Compare 0 with _x (bp-8)
+	cmpl	$0, _x@GOTOFF(%ebx)			# Compare 0 with _x (bp-8)
 	jle	ELSE				# if _x <= 0, jump to ELSE
 
 LOOP:						# loop while _x > 0
-	cmpl	$0, _x			# Compare 0 with _x (bp-8)
+	cmpl	$0, _x@GOTOFF(%ebx)			# Compare 0 with _x (bp-8)
 	jle	ENDIF				# when _x <= 0, jump to ENDIF
 							# out.writeint(x)
 	subl	$4, %esp		# prep for call
-	movl	_x, %eax		# push parameter (x)
+	movl	_x@GOTOFF(%ebx), %eax		# push parameter (x)
 	pushl	%eax			#	(continued)
 	call	writeint@PLT	# call stub for write
 	addl	$8, %esp		# clear parameters back off stack
-	subl	$1, _x			# _x := _x - 1
+	subl	$1, _x@GOTOFF(%ebx)			# _x := _x - 1
 	jmp		LOOP
 ELSE:						# out.writeint(999)
 	subl	$4, %esp		# prep for call
