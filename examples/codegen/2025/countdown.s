a750b432bafe7014af158fc0f0dddf7d877fc3d1
Sarah Gothard <sgothard@bju.edu>
2025-03-21 13:54:09 -0400

    Comments and simplification

--- examples/codegen/2025/countdown.s
+++ examples/codegen/2025/countdown.s
@@ -1,42 +1,42 @@
-    .comm x, 4, 4
-STDOUT: .long 1
-SIZE: .long 10
-    blastoff:
-    .string    "Blastoff!\n"
-    .globl    main
-main:
-    leal    4(%esp), %ecx    
-    pushl   -4(%ecx)    
-    pushl   %ebp    
-    movl    %esp, %ebp    
-    pushl   %ebx    
-    pushl   %ecx    
-    subl    $32, %esp    
-    call    __x86.get_pc_thunk.bx    
-    addl    $_GLOBAL_OFFSET_TABLE_, %ebx    
-    movl    $5, x@GOTOFF(%ebx)    
-LOOP:    
-    cmpl    $0, x@GOTOFF(%ebx)    
-    jle     ENDLOOP    
-    subl    $4, %esp    
-    movl    x@GOTOFF(%ebx), %eax    
-    pushl   %eax    
-    call    writeint@PLT    
-    addl    $8, %esp    
-    subl    $1, x@GOTOFF(%ebx)    
+    .comm x, 4, 4                          # allocate 4 bytes for x with 4-byte alignment
+STDOUT: .long 1                            # create a long named STDOUT initialized to 1
+SIZE: .long 10                             # create a long named SIZE initialized to 10
+    blastoff:                              # create a long named SIZE initialized to 10
+    .string    "Blastoff!\n"               # create a string named blastoff
+    .globl    main                         # main function (global)
+main:                                      # standard function prologue:
+    leal    4(%esp), %ecx                  # store address sp+4 in cx
+    pushl   -4(%ecx)                       # push sp
+    pushl   %ebp                           # push bp
+    movl    %esp, %ebp                     # store updated sp in bp.
+    pushl   %ebx                           # push bx
+    pushl   %ecx                           # push sp+4
+    subl    $32, %esp                      # reserve space on the stack. Stack grows down.
+    call    __x86.get_pc_thunk.bx          # put ip in bx. See https://courses.cs.vt.edu/cs3214/spring2022/questions/pcmaterialization        
+    addl    $_GLOBAL_OFFSET_TABLE_, %ebx   # set bx to _GLOBAL_OFFSET_TABLE_ + ip of <main+23>
+    movl    $10, x@GOTOFF(%ebx)            # x := 10
+LOOP:                                      # loop while _x > 0
+    cmpl    $0, x@GOTOFF(%ebx)             # Compare 0 with x   
+    jle     ENDLOOP                        # if x <= 0, jump to ENDIF
+    movl    x@GOTOFF(%ebx), %eax           # push parameter x
+    pushl   %eax                           #    (continued)
+    call    writeint@PLT                   # call stub for writeint
+    addl    $4, %esp                       # clear parameter back off stack
+    subl    $1, x@GOTOFF(%ebx)             # x := x - 1 
     jmp     LOOP
 ENDLOOP:    
-    subl    $4, %esp    
-    pushl   SIZE@GOTOFF(%ebx)
-    leal    blastoff@GOTOFF(%ebx), %eax  
-    pushl   %eax
-    pushl   STDOUT@GOTOFF(%ebx)
-    call    write@PLT    
-    addl    $16, %esp
-    leal    -8(%ebp), %esp    
-    popl    %ecx    
-    popl    %ebx    
-    popl    %ebp    
-    leal    -4(%ecx), %esp    
-    ret    
-    .section  .note.GNU-stack,"",@progbits    
+    pushl   SIZE@GOTOFF(%ebx)              # 1) push parameter SIZE
+    leal    blastoff@GOTOFF(%ebx), %eax    # 2) push parameter blastoff string
+    pushl   %eax                           #    (continued)
+    pushl   STDOUT@GOTOFF(%ebx)            # 3) push parameter STDOUT
+    call    write@PLT                      # call stub for write
+    addl    $12, %esp                      # clear parameters back off stack
+    movl    $0, %eax                       # zero out return value.
+                                           # standard function epilogue:
+    leal    -8(%ebp), %esp                 # restore original sp+4 to sp  
+    popl    %ecx                           # retrieve sp + 4 (see ln 13) 
+    popl    %ebx                           # restore bx (see ln 12)
+    popl    %ebp                           # restore bp (see ln 10)
+    leal    -4(%ecx), %esp                 # restore sp (see ln 9)
+    ret                                    # function exit
+    .section  .note.GNU-stack,"",@progbits # security feature to allow a non-executable stack 
--- examples/codegen/2025/loopy_var.s
+++ examples/codegen/2025/loopy_var.s
@@ -15,48 +15,46 @@
 #     end if
 #   end start
 # end loopy
-    .comm _x, 4, 4          # allocate 4 bytes for _x with 4-byte alignment
+    .comm x, 4, 4                          # allocate 4 bytes for x with 4-byte alignment
 
-.globl    main              # main function (global)
-main:                       # standard function prologue:
-    leal    4(%esp), %ecx   # store address sp+4 in cx
-    pushl   -4(%ecx)        # push sp
-    pushl   %ebp            # push bp
-    movl    %esp, %ebp      # store updated sp in bp.
-    pushl   %ebx            # push bx
-    pushl   %ecx            # push sp+4
-    subl    $32, %esp                        # reserve space on the stack. Stack grows down.
-    call    __x86.get_pc_thunk.bx            # put ip in bx. See https://courses.cs.vt.edu/cs3214/spring2022/questions/pcmaterialization        
-    addl    $_GLOBAL_OFFSET_TABLE_, %ebx     # set bx to _GLOBAL_OFFSET_TABLE_ + ip of <main+23>
-    movl    $3, _x@GOTOFF(%ebx)              # _x := 3
-                                             # if (x > 0) then
-    cmpl    $0, _x@GOTOFF(%ebx)              # Compare 0 with _x (bp-8)
-    jle     ELSE                             # if _x <= 0, jump to ELSE
+    .globl    main                         # main function (global)
+main:                                      # standard function prologue:
+    leal    4(%esp), %ecx                  # store address sp+4 in cx
+    pushl   -4(%ecx)                       # push sp
+    pushl   %ebp                           # push bp
+    movl    %esp, %ebp                     # store updated sp in bp.
+    pushl   %ebx                           # push bx
+    pushl   %ecx                           # push sp+4
+    subl    $32, %esp                      # reserve space on the stack. Stack grows down.
+    call    __x86.get_pc_thunk.bx          # put ip in bx. See https://courses.cs.vt.edu/cs3214/spring2022/questions/pcmaterialization        
+    addl    $_GLOBAL_OFFSET_TABLE_, %ebx   # set bx to _GLOBAL_OFFSET_TABLE_ + ip of <main+23>
+    movl    $3, x@GOTOFF(%ebx)             # x := 3
+                                           # if (x > 0) then
+    cmpl    $0, x@GOTOFF(%ebx)             # Compare 0 with x (bp-8)
+    jle     ELSE                           # if x <= 0, jump to ELSE
 
-LOOP:                             # loop while _x > 0
-    cmpl    $0, _x@GOTOFF(%ebx)   # Compare 0 with _x (bp-8)
-    jle     ENDIF                 # when _x <= 0, jump to ENDIF
-                                  # out.writeint(_x)
-    subl    $4, %esp              # prep for call
-    movl    _x@GOTOFF(%ebx), %eax # push parameter (_x)
-    pushl   %eax                  #    (continued)
-    call    writeint@PLT          # call stub for write
-    addl    $8, %esp              # clear parameters back off stack
-    subl    $1, _x@GOTOFF(%ebx)   # _x := _x - 1
+LOOP:                                      # loop while x > 0
+    cmpl    $0, x@GOTOFF(%ebx)             # Compare 0 with x
+    jle     ENDIF                          # when x <= 0, jump to ENDIF
+                                           # out.writeint(_x)
+    movl    x@GOTOFF(%ebx), %eax           # push parameter (_x)
+    pushl   %eax                           #    (continued)
+    call    writeint@PLT                   # call stub for writeint
+    addl    $4, %esp                       # clear parameter back off stack
+    subl    $1, x@GOTOFF(%ebx)             # x := x - 1
     jmp     LOOP
-ELSE:                             # out.writeint(999)
-    subl    $4, %esp              # prep for call
-    movl    $999, %eax            # push parameter
-    pushl   %eax                  #    (continued)
-    call    writeint@PLT          # call stub for write
-    addl    $8, %esp              # clear parameters back off stack
+ELSE:                                      # out.writeint(999)
+    movl    $999, %eax                     # push parameter
+    pushl   %eax                           #    (continued)
+    call    writeint@PLT                   # call stub for write
+    addl    $4, %esp                       # clear parameters back off stack
+    movl    $0, %eax                       # zero out return value.
 ENDIF:
-                                  # standard function epilogue:
-    movl    $0, %eax              # zero out return value.
-    leal    -8(%ebp), %esp        # restore original sp+4 to sp
-    popl    %ecx                  # retrieve sp + 4 (see ln 9)
-    popl    %ebx                  # restore bx (see ln 8)
-    popl    %ebp                  # restore bp (see ln 6)
-    leal    -4(%ecx), %esp        # restore sp (see ln 4)
-    ret                           # function exit
-    .section  .note.GNU-stack,"",@progbits    # security feature to allow a non-executable stack 
+                                           # standard function epilogue:
+    leal    -8(%ebp), %esp                 # restore original sp+4 to sp
+    popl    %ecx                           # retrieve sp + 4 (see ln 27)
+    popl    %ebx                           # restore bx (see ln 26)
+    popl    %ebp                           # restore bp (see ln 24)
+    leal    -4(%ecx), %esp                 # restore sp (see ln 23)
+    ret                                    # function exit
+    .section  .note.GNU-stack,"",@progbits # security feature to allow a non-executable stack 
