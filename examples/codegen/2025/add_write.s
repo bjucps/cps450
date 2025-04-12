fcefcddf9bef702d3b0569a02d4c8ea6effa28c1
doctor_sarah <sgothard@bju.edu>
2025-04-11 21:18:24 -0400

    padding added

--- examples/codegen/2025/add_write.s
+++ examples/codegen/2025/add_write.s
@@ -3,32 +3,32 @@
 # int main(int argc, char **argv) {
 #     int x = 6;
 #     int y = 3;
-#     char c = x + y + '0'; // 48 is the unicode value, 0x30 in hex 
+#     char c = x + y + '0'; // 48 is the unicode value, 0x30 in hex
 #     write(1, &c, 1);
 # }
 
 STDOUT = 1
-.globl    main
-main:
+.globl    main                            # main function (global)
+main:                                     # standard function prologue:
     pushl   %ebp                          # store the original bp.
     movl    %esp, %ebp                    # set bp.
     pushl   %ebx                          # store original bx (at bp-4)
     pushl   %ecx                          # store original cx (at bp-8).
-    pushl   %edx                          # store original dx (at bp-12); local variables begin at bp-16.
-                                          # _GLOBAL_OFFSET_TABLE needed for write function call.
-    call    __x86.get_pc_thunk.ax         # put ip in ax. See https://courses.cs.vt.edu/cs3214/spring2022/questions/pcmaterialization        
+    pushl   %edx                          # store original dx (at bp-12); local variables begin at bp-20.
+    subl    $12, %esp                     # reserve space on the stack for the global offset table.
+    call    __x86.get_pc_thunk.ax         # put ip in ax. See https://courses.cs.vt.edu/cs3214/spring2022/questions/pcmaterialization
     addl    $_GLOBAL_OFFSET_TABLE_, %eax  # set ax to _GLOBAL_OFFSET_TABLE_ + ip of <main+something>
-    movl    $6, -16(%ebp)                 # x (bp-16)= 6
-    movl    -16(%ebp), %ecx               
-    movl    $3, -20(%ebp)                 # y (bp-20)= 3
-    movl    -20(%ebp), %edx               
+    movl    $6, -20(%ebp)                 # x (bp-20)= 6
+    movl    -20(%ebp), %ecx
+    movl    $3, -24(%ebp)                 # y (bp-24)= 3
+    movl    -24(%ebp), %edx
     addl    %ecx, %edx                    # add 6 to 3
     addl    $48, %edx                     # add '0' to sum
-    movl    %edx, -24(%ebp)               # c (bp-24) = sum
+    movl    %edx, -28(%ebp)               # c (bp-28) = sum
     subl    $4, %esp                      # subtract padding so that parameter + padding is a multiple of 16.
                                           # parameters:
     pushl   $1                            #    3) 1 (number of bytes to write)
-    leal    -24(%ebp), %edx               #    2) Address of '9'
+    leal    -28(%ebp), %edx               #    2) Address of '9'
     pushl   %edx                          #         (continued)
     pushl   $STDOUT                       #    1) STDOUT
     movl    %eax, %ebx                    # backup ax
@@ -42,5 +42,5 @@ main:
     popl    %ecx                          # restore cx
     popl    %ebx                          # restore bx
     leave                                 # equal to mov %ebp, %esp; pop %ebp.
-    ret                                   
-    .section .note.GNU-stack,"",@progbits # security feature to allow a non-executable stack 
+    ret
+    .section .note.GNU-stack,"",@progbits # security feature to allow a non-executable stack
--- examples/codegen/2025/countdown.s
+++ examples/codegen/2025/countdown.s
@@ -1,27 +1,27 @@
     .comm x, 4, 4                         # allocate 4 bytes for x with 4-byte alignment
 STDOUT: .long 1                           # create a long named STDOUT initialized to 1
 SIZE: .long 10                            # create a long named SIZE initialized to 10
-    blastoff:                             
+    blastoff:
     .string    "Blastoff!\n"              # create a string named blastoff
     .globl    main                        # main function (global)
 main:                                     # standard function prologue:
     pushl   %ebp                          # store the original bp.
     movl    %esp, %ebp                    # set bp.
     pushl   %ebx                          # store original bx (at bp-4); local variables begin at bp-8.
-    call    __x86.get_pc_thunk.bx         # put ip in bx. See https://courses.cs.vt.edu/cs3214/spring2022/questions/pcmaterialization        
+    call    __x86.get_pc_thunk.bx         # put ip in bx. See https://courses.cs.vt.edu/cs3214/spring2022/questions/pcmaterialization
     addl    $_GLOBAL_OFFSET_TABLE_, %ebx  # set bx to _GLOBAL_OFFSET_TABLE_ + ip of <main+something>
     movl    $10, x@GOTOFF(%ebx)           # x := 10
 LOOP:                                     # loop while _x > 0
-    cmpl    $0, x@GOTOFF(%ebx)            # Compare 0 with x   
+    cmpl    $0, x@GOTOFF(%ebx)            # Compare 0 with x
     jle     ENDLOOP                       # if x <= 0, jump to ENDIF
     subl    $12, %esp                     # subtract padding so that parameter + padding is a multiple of 16.
     movl    x@GOTOFF(%ebx), %eax          # push parameter x
-    pushl   %eax                           
+    pushl   %eax
     call    writeint@PLT                  # call stub for writeint
     addl    $16, %esp                     # clean off stack after call (always a multiple of 16).
-    subl    $1, x@GOTOFF(%ebx)            # x := x - 1 
+    subl    $1, x@GOTOFF(%ebx)            # x := x - 1
     jmp     LOOP
-ENDLOOP:    
+ENDLOOP:
     subl    $4, %esp                      # subtract padding so that parameter + padding is a multiple of 16.
     pushl   SIZE@GOTOFF(%ebx)             # 1) push parameter SIZE
     leal    blastoff@GOTOFF(%ebx), %eax   # 2) push parameter blastoff string
@@ -34,4 +34,4 @@ ENDLOOP:
     popl    %ebx                          # restore bx
     leave                                 # equal to mov %ebp, %esp; pop %ebp.
     ret
-    .section .note.GNU-stack,"",@progbits # security feature to allow a non-executable stack 
+    .section .note.GNU-stack,"",@progbits # security feature to allow a non-executable stack
